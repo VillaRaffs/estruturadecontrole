243ecebbb18e78d562a79bfd258a4b19085c06bd
Rafaela Villatore <morango20n1@gmail.com>
2025-10-02 08:08:09 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+//Lógica de verificação de idade
+
+const idadeInput = document.getElementById('idadeInput');
+const verifyButton = document.getElementById('verifyButton');
+const result = document.getElementById('resultado');
+
+function verificarIdade(){
+    resultado.classList.remove('visivel');
+    const idade = parseInt(idadeInput.value);
+    let menssagem = '';
+
+    if(isNaN(idade)|| idade < 0){
+        menssagem = 'Por favor, insira uma idade válida.';
+    } else if(idade < 18){
+        menssagem = 'Você é menor de idade';
+    }else if(idade < 65){
+        
+    }
+}
